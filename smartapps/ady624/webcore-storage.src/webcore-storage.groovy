757e51848576947c237e12ee901934a175149602
ipaterson <ian@ipaterson.com>
2018-09-14 19:35:23 -0400

    Added exception logging to listAvailableDevices to find the original exception behind the UndeclaredThrowableException

--- smartapps/ady624/webcore-storage.src/webcore-storage.groovy
+++ smartapps/ady624/webcore-storage.src/webcore-storage.groovy
@@ -136,7 +136,28 @@ def Map listAvailableDevices(raw = false) {
 	if (raw) {
     	return settings.findAll{ it.key.startsWith("dev:") }.collect{ it.value }.flatten().collectEntries{ dev -> [(hashId(dev.id)): dev]}
     } else {
-    	return settings.findAll{ it.key.startsWith("dev:") }.collect{ it.value }.flatten().collectEntries{ dev -> [(hashId(dev.id)): dev]}.collectEntries{ id, dev -> [ (id): [ n: dev.getDisplayName(), cn: dev.getCapabilities()*.name, a: dev.getSupportedAttributes().unique{ it.name }.collect{def x = [n: it.name, t: it.getDataType(), o: it.getValues()]; try {x.v = dev.currentValue(x.n);} catch(all) {}; x}, c: dev.getSupportedCommands().unique{ it.getName() }.collect{[n: it.getName(), p: it.getArguments()]} ]]}
+			try { 
+	    	def results = settings.findAll{ it.key.startsWith("dev:") }.collect{ it.value }.flatten().collectEntries{ dev -> [(hashId(dev.id)): dev]}.collectEntries{ id, dev -> [ (id): [ 
+					n: dev.getDisplayName(), 
+					cn: dev.getCapabilities()*.name, 
+					a: dev.getSupportedAttributes().unique{ it.name }.collect{
+						def x = [
+							n: it.name, 
+							t: it.getDataType(), 
+							o: it.getValues()
+						]
+						try {x.v = dev.currentValue(x.n);} catch(all) {}
+						x
+					}, 
+					c: dev.getSupportedCommands().unique{ it.getName() }.collect{[
+						n: it.getName(), 
+						p: it.getArguments()
+					]} 
+				]]}
+				log.debug "Loaded ${results.size()} devices"
+			} catch (e) {
+				log.error "Failed to load devices", e
+			}
 	}
 }
 
